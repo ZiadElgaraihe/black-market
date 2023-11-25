ebeae37bd83322aed53da98844d7de46d0107d57
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2023-11-25 13:44:37 +0200

    create dio helper class

---
+++ lib/core/helpers/dio_helper.dart
@@ -0,0 +1,61 @@
+import 'package:dio/dio.dart';
+
+class DioHelper {
+  DioHelper() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://voipsys.space/api/',
+      ),
+    );
+  }
+
+  late Dio _dio;
+
+  ///http get request
+  Future<Map<String, dynamic>> getRequest({
+    required String endPoint,
+    Map<String, dynamic>? queryParameters,
+    String? token,
+  }) async {
+    Map<String, dynamic>? headers;
+
+    if (token != null) {
+      headers = {'Authorization': 'Bearer $token'};
+    }
+
+    Response response = await _dio.get(
+      endPoint,
+      queryParameters: queryParameters,
+      options: Options(
+        headers: headers,
+      ),
+    );
+
+    return response.data;
+  }
+
+  ///http post request
+  Future<Map<String, dynamic>> postRequest({
+    required String endPoint,
+    Object? data,
+    String? token,
+  }) async {
+    Map<String, dynamic> headers = {
+      'Content-Type': 'application/json',
+    };
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+
+    Response response = await _dio.post(
+      endPoint,
+      data: data,
+      options: Options(
+        headers: headers,
+      ),
+    );
+
+    return response.data;
+  }
+}
