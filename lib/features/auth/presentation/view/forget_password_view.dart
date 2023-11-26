885d3419f78fd05ba0331df5133e81c6c9370493
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2023-11-26 21:05:23 +0200

    add forget password cubit on the material app

--- lib/features/auth/presentation/view/forget_password_view.dart
+++ lib/features/auth/presentation/view/forget_password_view.dart
@@ -1,26 +1,17 @@
 import 'package:black_market/core/presentation/view/app_bars/default_app_bar.dart';
-import 'package:black_market/core/utils/service_locator.dart';
-import 'package:black_market/features/auth/data/services/auth_services.dart';
 import 'package:black_market/features/auth/presentation/view/widgets/forget_password_view_body.dart';
-import 'package:black_market/features/auth/presentation/view_model/forget_password_cubit/forget_password_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ForgetPasswordView extends StatelessWidget {
   const ForgetPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ForgetPasswordCubit>(
-      create: (context) => ForgetPasswordCubit(
-        authServices: getIt<AuthServices>(),
-      ),
-      child: GestureDetector(
-        onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
-        child: const Scaffold(
-          appBar: DefaultAppBar(title: 'إسترجاع كلمة المرور'),
-          body: ForgetPasswordViewBody(),
-        ),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
+      child: const Scaffold(
+        appBar: DefaultAppBar(title: 'إسترجاع كلمة المرور'),
+        body: ForgetPasswordViewBody(),
       ),
     );
   }
