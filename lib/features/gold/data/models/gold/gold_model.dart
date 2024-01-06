63fda566031f535e112f87b2787df36b78b3f388
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-01-06 19:43:34 +0200

    create gold model and gold price model

---
+++ lib/features/gold/data/models/gold/gold_model.dart
@@ -0,0 +1,24 @@
+part 'gold_price_model.dart';
+
+class GoldModel {
+  int id;
+  String name;
+  String karat;
+  GoldPriceModel price;
+
+  GoldModel({
+    required this.id,
+    required this.name,
+    required this.karat,
+    required this.price,
+  });
+
+  factory GoldModel.fromJson({required Map<String, dynamic> data}) {
+    return GoldModel(
+      id: data['id'],
+      name: data['name'],
+      karat: data['karat'],
+      price: data['price'],
+    );
+  }
+}
