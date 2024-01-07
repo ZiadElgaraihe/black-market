3b8092fbb54470ff6b63a89f48e0a40cae73750b
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-01-07 18:39:03 +0200

    create get ingots coins data cubit

---
+++ lib/features/gold/presentation/view_model/get_ingots_coins_data_cubit/get_ingots_coins_data_cubit.dart
@@ -0,0 +1,65 @@
+import 'package:black_market/core/data/services/connection_services.dart';
+import 'package:black_market/core/errors/failure.dart';
+import 'package:black_market/features/gold/data/models/ingots_coins/ingots_coins_model.dart';
+import 'package:black_market/features/gold/data/repos/gold_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'get_ingots_coins_data_state.dart';
+
+class GetIngotsCoinsDataCubit extends Cubit<GetIngotsCoinsDataState> {
+  GetIngotsCoinsDataCubit({
+    required ConnectionServices connectionServices,
+    required GoldServices goldServices,
+  }) : super(GetIngotsCoinsDataInitial()) {
+    _connectionServices = connectionServices;
+    _goldServices = goldServices;
+  }
+
+  late ConnectionServices _connectionServices;
+  late GoldServices _goldServices;
+
+  List<IngotsModel>? ingots;
+  List<GoldenCoinsModel>? goldenCoins;
+
+  Future<void> getIngotsCoinsData({bool isRefresh = false}) async {
+    if (!isRefresh && ingots != null && goldenCoins != null) {
+      emit(GetIngotsCoinsDataSuccess());
+    } else {
+      Either<ConnectionFailure, void> connectionResult =
+          await _connectionServices.checkInternetConnection();
+
+      connectionResult.fold(
+        //no connection
+        (connectionFailure) {
+          emit(
+            GetIngotsCoinsDataFailure(errMessage: connectionFailure.errMessage),
+          );
+        },
+        //connection
+        (_) async {
+          Either<Failure, IngotsCoinsModel> result =
+              await _goldServices.getIngotsCoinsData();
+
+          result.fold(
+            //error
+            (failure) {
+              emit(
+                GetIngotsCoinsDataFailure(errMessage: failure.errMessage),
+              );
+            },
+            //success
+            (ingotsCoinsModel) {
+              ingots = ingotsCoinsModel.ingots;
+              goldenCoins = ingotsCoinsModel.coins;
+              emit(
+                GetIngotsCoinsDataSuccess(),
+              );
+            },
+          );
+        },
+      );
+    }
+  }
+}
