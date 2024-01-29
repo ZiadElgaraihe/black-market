bcccaf156e043e9d70ea2f2c432cb5d31b403274
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-01-29 21:24:51 +0200

    launch notification link if not null

--- lib/features/notifications/presentation/view/widgets/columns/notifications_date_section_column.dart
+++ lib/features/notifications/presentation/view/widgets/columns/notifications_date_section_column.dart
@@ -1,7 +1,12 @@
+import 'package:black_market/core/functions/show_alert_dialog.dart';
+import 'package:black_market/core/localization/generated/l10n.dart';
+import 'package:black_market/core/presentation/view/alert_dialogs/result_alert_dialog.dart';
 import 'package:black_market/features/notifications/data/models/notification_model.dart';
 import 'package:black_market/features/notifications/presentation/view/widgets/dividers/date_divider.dart';
 import 'package:black_market/features/notifications/presentation/view/widgets/rows/notification_row.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NotificationsDateSectionColumn extends StatelessWidget {
   const NotificationsDateSectionColumn({
@@ -23,12 +28,38 @@ class NotificationsDateSectionColumn extends StatelessWidget {
         Column(
           children: List.generate(
             notifications.length,
-            (index) => NotificationRow(
-              notification: notifications[index],
+            (index) => InkWell(
+              onTap: (notifications[index].link != null)
+                  ? () async {
+                      await _launchNotificationLink(context, index);
+                    }
+                  : null,
+              borderRadius: BorderRadius.circular(14.w),
+              child: NotificationRow(
+                notification: notifications[index],
+              ),
             ),
           ),
         ),
       ],
     );
+  }
+
+  Future<void> _launchNotificationLink(BuildContext context, int index) async {
+    Uri uri = Uri.parse(notifications[index].link!);
+    if (!await launchUrl(uri)) {
+      if (!context.mounted) return;
+      showAlertDialog(
+        context,
+        child: ResultAlertDialog(
+          buttonTitle: Tr.of(context).cancel,
+          message: 'Can\'t Launch This Url',
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          title: Tr.of(context).failure,
+        ),
+      );
+    }
   }
 }
