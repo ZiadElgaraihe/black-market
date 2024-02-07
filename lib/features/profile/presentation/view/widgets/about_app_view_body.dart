42648d8b352578b7c06db2eb07d5c93c3edd9089
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-02-07 22:05:31 +0200

    modify about app view

--- lib/features/profile/presentation/view/widgets/about_app_view_body.dart
+++ lib/features/profile/presentation/view/widgets/about_app_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:black_market/core/utils/app_colors.dart';
+import 'package:black_market/core/utils/constants.dart';
 import 'package:black_market/core/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -12,7 +13,7 @@ class AboutAppViewBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 24.w),
       child: Column(
         children: [
-          SizedBox(height: 25.h),
+          const Spacer(flex: 1),
           Text(
             'Black Market',
             style: TextStyles.textStyle28.copyWith(
@@ -26,19 +27,21 @@ class AboutAppViewBody extends StatelessWidget {
               color: AppColors.lightYellowActive,
             ),
           ),
-          const Spacer(),
+          const Spacer(flex: 1),
           Text(
             '''
-      تطبيق " بكام في السوق السوداء " والقائمين عليه وشركة Flux Tech ىمالكة التطبيق لا يقومون بالتجارة في العملة ولا يشجعون عليها... بل بالعكس تماما نشجع على الالتزام بالسوق الرسمي للعملات حفاظا علي أموالكم\n
+      تطبيق " بكام في السوق السوداء " والقائمين عليه وشركة Flux Tech مالكة التطبيق لا يقومون بالتجارة في العملة ولا يشجعون عليها... بل بالعكس تماما نشجع على الالتزام بالسوق الرسمي للعملات حفاظا علي أموالكم\n
       مع العلم أيضا أن الأسعار في التطبيق هي أسعار تقريبية للاسترشاد بها فقط لذلك الرجاء عدم الاعتماد عليها بشكل أساسي في حساب أموالكم\n
       ولا نتحمل أي مسؤولية مادية أو قانونية نتيجة الاعتماد عليها لأن الأرقام يتم تجميعها من وسائل الإعلام العامة والخاصة التي قد تكون غير دقيقة.\n
       تنويه هام..
       برجاء العلم أن تعاملات السوق السوداء غير قانونية ومجرمة بحكم القانون المصري ويفرض على القائم بها أو من يعمل في السوق السوداء بإجراءت عقابية وفوق كل هذا يعتبر إضرار بالأمن القومي لاقتصاد مصر والأمن القومي لاقتصادك كمواطن.
       ''',
-            style: TextStyles.textStyle12,
+            style: TextStyles.textStyle12.copyWith(
+              height: isTablet ? 1.h : 2.h,
+            ),
             textAlign: TextAlign.center,
           ),
-          const Spacer(),
+          const Spacer(flex: 2),
         ],
       ),
     );
